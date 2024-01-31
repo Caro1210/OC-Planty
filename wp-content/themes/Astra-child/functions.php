dcb377009ca24a5efcefbe0144e734c960eeb224
Caro1210 <c.delannoy12@gmx.com>
2024-01-31 14:37:44 +0100

    hook add menu item admin

--- wp-content/themes/Astra-child/functions.php
+++ wp-content/themes/Astra-child/functions.php
@@ -8,4 +8,13 @@ function theme_enqueue_styles() {
     // Enregistrement du style du thème enfant avec une dépendance sur le style du thème parent
     wp_enqueue_style( 'theme-style', get_stylesheet_directory_uri() . '/css/theme.css', array('parent-theme'), filemtime( get_stylesheet_directory() . '/css/theme.css' ) );
 }
+
+/* Ajout du hook*/
+add_filter( 'wp_nav_menu_items','add_admin_link', 10, 2 );
+function add_admin_link( $items, $args ) {
+if (is_user_logged_in() ) {
+$items .= '<li id="menu-item-73" class="menu-item menu-item-type-post_type menu-item-object-page menu-item-73"><a href="'. get_admin_url() .'">Admin</a></li>';
+}
+return $items;
+}
 ?>
