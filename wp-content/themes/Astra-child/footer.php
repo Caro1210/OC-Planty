7d37b1f7e32ec03e4594ed79ad0362ad76b30dff
Caro1210 <c.delannoy12@gmx.com>
2024-02-01 00:20:12 +0100

    Mise à jour du hook qui apparaissait dans le et reprogrammation du footer

--- wp-content/themes/Astra-child/footer.php
+++ wp-content/themes/Astra-child/footer.php
@@ -22,13 +22,21 @@ if ( ! defined( 'ABSPATH' ) ) {
 	astra_content_after();
 		
 	astra_footer_before();
-		
+	
 	astra_footer();
-		
+		// Ajout du lien vers la page "Mentions légales"
+$mentions_legales_id = get_page_by_title('Mentions légales')->ID;
+if ($mentions_legales_id) {
+    echo '<div class="mentions-legales-link">';
+    echo '<a href="' . get_permalink($mentions_legales_id) . '">Mentions légales</a>';
+    echo '</div>';
+}
+			
 	astra_footer_after(); 
 ?>
 	</div><!-- #page -->
 <?php 
+
 	astra_body_bottom();    
 	wp_footer(); 
 ?>
